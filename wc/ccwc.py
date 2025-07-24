69f845adf80d720e8814773c088a40fa4697ecbb
Mohammed-Faizzzz <110959467+Mohammed-Faizzzz@users.noreply.github.com>
2025-07-24 12:16:40 +0100

    Add count_lines and count_words

--- wc/ccwc.py
+++ wc/ccwc.py
@@ -19,6 +19,40 @@ def count_bytes(file_path):
         sys.exit
     return 
 
+def count_lines(file_path):
+    # open file
+    line_count = 0
+    try:
+        with open(file_path, 'rb') as f:
+            for line in f:
+                line_count += 1
+        return line_count
+    except FileNotFoundError:
+        print("Please provide a valid file.")
+        sys.exit
+    except IOError:
+        print("Error reading file.")
+        sys.exit
+    return 
+
+def count_words(file_path):
+    # open file
+    word_count = 0
+    try:
+        with open(file_path, 'r', encoding='utf-8') as f: # read without binary
+            for line in f:
+                line = line.split()
+                # print(line)
+                word_count += len(line)
+        return word_count
+    except FileNotFoundError:
+        print("Please provide a valid file.")
+        sys.exit
+    except IOError:
+        print("Error reading file.")
+        sys.exit
+    return 
+
 def main():
     if len(sys.argv) < 3: # not enough arguments
         print("Usage: ccwc -c <file>")
@@ -28,6 +62,12 @@ def main():
     if command == '-c':
         byte_count = count_bytes(file)
         print(f"{byte_count}\t{file}") # Standard wc output format: count TAB filename
+    elif command == '-l':
+        line_count = count_lines(file)
+        print(f"{line_count}\t{file}")
+    elif command == '-w':
+        word_count = count_words(file)
+        print(f"{word_count}\t{file}")
     else:
         print(f"ccwc: invalid option -- '{command}'", file=sys.stderr)
         print("Try 'ccwc --help' for more information.", file=sys.stderr)
