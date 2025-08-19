5ce4bbce12c5ae3f89fbdd8a3134061730a4a40a
Mohammed-Faizzzz <110959467+Mohammed-Faizzzz@users.noreply.github.com>
2025-08-19 12:00:02 +0100

    Complete Step 2

--- cut/my_cut.py
+++ cut/my_cut.py
@@ -6,20 +6,26 @@ import os  # to interact with the operating system
 
 def handle_3_args(field, file):
     with open(file, 'r', encoding='utf-8') as f:
+        field = int(field[2])-1
         for line in f:
             line = line.split()
-            print(line[1])
+            print(line[field])
+            
+def handle_4_args(field, delimiter, file):
+    with open(file, 'r', encoding='utf-8') as f:
+        field = int(field[2])-1
+        delimiter = delimiter[2]
+        for line in f:
+            line = line.split(delimiter)
+            print(line[field])
 
 def main():
     if len(sys.argv) == 3:
         field, file = sys.argv[1], sys.argv[2]
         handle_3_args(field, file)
-    # elif len(sys.argv == 2):
-    #     option = sys.argv[1]
-    #     if option == '-l' or option == '-w' or option == '-c' or option == '-m':
-    #         handle_cmdonly(option)
-    #     else:
-    #         handle_fileonly(option)
+    elif len(sys.argv) == 4:
+        field, delimiter, file = sys.argv[1], sys.argv[2], sys.argv[3]
+        handle_4_args(field, delimiter, file)
     else:
         print("Error: wrong command")
         sys.exit(1)    
